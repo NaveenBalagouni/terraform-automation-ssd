d74a41a65782f9314d5074638e5a2381f58a0eec
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-15 15:38:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ssd_instance" {
   source        = "./ssd-instance"
-  ssd_namespace = "ssd-terraform"
-  release_name  = "opsmx-ssd-terraform"
-
-  values_file = "${path.cwd}/enterprise-ssd/charts/ssd/ssd-minimal-values.yaml"
+  ssd_namespace = var.target_namespace
+  release_name  = var.ssd_release_name
+  # Points to the values file inside the cloned repo
+  values_file   = "${path.cwd}/enterprise-ssd/charts/ssd/ssd-minimal-values.yaml"
 }
