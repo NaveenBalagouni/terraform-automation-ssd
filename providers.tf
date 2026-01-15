08a5e99f4de9d454df031e8997dd39c60f634aa7
NaveenBalagouni <159781517+NaveenBalagouni@users.noreply.github.com>
2026-01-15 15:37:45 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,14 +1,23 @@
 terraform {
   required_version = ">= 1.5.0"
-
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 3.0"
+      version = "~> 2.23"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 3.0"
+      version = "~> 2.11"
     }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config" # Change this for CI/CD environments
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
   }
 }
